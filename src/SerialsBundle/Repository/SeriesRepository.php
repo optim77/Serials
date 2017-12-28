a566cb5de0cbcc4a66fd52cc2850588c7336846e
kuba <plajerowy@gmail.com>
2017-12-28 14:33:51 +0100

    Series Entity

---
+++ src/SerialsBundle/Repository/SeriesRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NASA
+ * Date: 2017-12-28
+ * Time: 13:55
+ */
+
+namespace SerialsBundle\Repository;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Doctrine\ORM\EntityRepository;
+use SerialsBundle\Entity\Series;
+
+class SeriesRepository extends EntityRepository
+{
+
+}
